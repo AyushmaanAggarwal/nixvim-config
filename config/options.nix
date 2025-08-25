1ae929ed3483692791fb95fa59adc855d6207a30
AyushmaanAggarwal <ayu02468@gmail.com>
2025-08-24 18:39:51 -0700

    Add several keybindings

--- config/options.nix
+++ config/options.nix
@@ -4,11 +4,13 @@
 
   # Options
   enableMan = true;
+
   globals = {
     mapleader = " ";
     maplocalleader = " ";
     have_nerd_font = true;
   };
+
   opts = {
     # Line Numbers
     number = true;
@@ -21,6 +23,8 @@
     # Cases in search
     ignorecase = true;
     smartcase = true;
+    hlsearch = true;
+
     signcolumn = "yes";
     updatetime = 250;
     timeoutlen = 300;
@@ -44,4 +48,47 @@
     spell = true;
     fileformats = "unix,dos,mac";
   };
+
+  keymaps = [
+    # Clear highlight on search
+    {
+      mode = "n";
+      key = "<Esc>";
+      action = "<cmd>nohlsearch<CR>";
+    }
+    # Diagnostics
+    {
+      mode = "n";
+      key = "[d";
+      action = "vim.diagnostic.goto_prev";
+      options = { desc = "Go to previous [D]iagnostic message"; };
+    }
+    {
+      mode = "n";
+      key = "]d";
+      action = "vim.diagnostic.goto_next";
+      options = { desc = "Go to next [D]iagnostic message"; };
+    }
+    {
+      mode = "n";
+      key = "<leader>e";
+      action = "vim.diagnostic.open_float";
+      options = { desc = "Show diagnostic [E]rror message"; };
+    }
+    {
+      mode = "n";
+      key = "<leader>q";
+      action = "vim.diagnostic.setloclist";
+      options = { desc = "Open diagnostic [Q]uickfix list"; };
+    }
+
+    # Exit terminal mode
+    {
+      mode = "t";
+      key = "<Esc><Esc>";
+      action = "<C-\\><C-n>";
+      options = { desc = "Exit terminal mode"; };
+    }
+
+  ];
 }
