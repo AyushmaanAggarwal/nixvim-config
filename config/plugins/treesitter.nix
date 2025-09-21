374f7dae025268c0e44871fb9b3ce8e957e53b5d
AyushmaanAggarwal <ayu02468@gmail.com>
2025-09-21 15:41:47 -0700

    feat: Change from vim-plugs to newer tree-sitter-grammars

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -2,7 +2,6 @@
 {
   extraPackages = with pkgs; [
     gcc
-    tree-sitter-grammars.tree-sitter-verilog
   ];
 
   # Highlight, edit, and navigate code
@@ -10,12 +9,37 @@
   plugins.treesitter = {
     enable = true;
 
-    settings = {
-      # Installing tree-sitter grammars from nvim-treesitter
-      # (can be combined with grammarPackages from Nixpkgs)
-      # https://nix-community.github.io/nixvim/plugins/treesitter/index.html#installing-tree-sitter-grammars-from-nvim-treesitter
-      ensureInstalled = [ ];
+    grammarPackages = with pkgs.tree-sitter-grammars; [
+      # Shell
+      tree-sitter-bash
 
+      # Data
+      tree-sitter-css
+      tree-sitter-json
+      tree-sitter-toml
+      tree-sitter-yaml
+      tree-sitter-html
+
+      # Languages
+      tree-sitter-lua
+      tree-sitter-nix
+      tree-sitter-rust
+      tree-sitter-python
+      tree-sitter-verilog
+      tree-sitter-hyprlang
+      tree-sitter-dockerfile
+
+      # Other
+      tree-sitter-make
+      tree-sitter-vim
+      tree-sitter-nvim
+      tree-sitter-regex
+      tree-sitter-latex
+      tree-sitter-markdown
+      tree-sitter-markdown-inline
+    ];
+
+    settings = {
       highlight.enable = true;
       indent.enable = true;
     };
