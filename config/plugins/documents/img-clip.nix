8b1877a64502e87ac2e6cd30aa9b4f883bde2539
AyushmaanAggarwal <ayu02468@gmail.com>
2025-09-29 23:19:37 -0700

    feat: Add specified template for img-clip

--- config/plugins/documents/img-clip.nix
+++ config/plugins/documents/img-clip.nix
@@ -12,6 +12,30 @@
 
       filetypes = {
         markdown = {
+          template = "![$CURSOR]($FILE_PATH)";
+          download_images = true;
+        };
+
+        vimwiki = {
+          template = "![$CURSOR]($FILE_PATH)";
+          download_images = true;
+        };
+
+        html = {
+          template = ''img src="$FILE_PATH" alt="$CURSOR">'';
+        };
+
+        tex = {
+          relative_template_path = true;
+          template = ''
+            [[
+            \begin{figure}[h]
+              \centering
+              \includegraphics[width=0.8\textwidth]{$FILE_PATH}
+              \caption{$CURSOR}
+              \label{fig:$LABEL}
+            \end{figure}
+            ]]'';
           download_images = true;
         };
       };
