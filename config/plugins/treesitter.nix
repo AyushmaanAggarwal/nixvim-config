b36a1f197e7a5cdd3e0d14093eb20f533201e943
AyushmaanAggarwal <ayu02468@gmail.com>
2026-03-24 23:52:08 -0700

    remove verilog from treesitter

--- config/plugins/treesitter.nix
+++ config/plugins/treesitter.nix
@@ -9,7 +9,6 @@ in
 
   extraPlugins = [
     # Languages
-    tree-sitter-verilog
   ];
 
   # Highlight, edit, and navigate code
@@ -19,25 +18,11 @@ in
 
     grammarPackages = pkgs.vimPlugins.nvim-treesitter.passthru.allGrammars ++ [
       # Languages
-      tree-sitter-verilog
     ];
 
     settings = {
       highlight.enable = true;
       indent.enable = true;
     };
-    luaConfig.post = ''
-      do
-        local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-        -- change the following as needed
-        parser_config.verilog = {
-          install_info = {
-            url = "${tree-sitter-verilog}",
-            files = {"src/parser.c"}, -- note that some parsers also require src/scanner.c or src/scanner.cc
-          },
-          filetype = "verilog", -- if filetype does not match the parser name
-        }
-      end
-    '';
   };
 }
